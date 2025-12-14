c17b725abbcf705d74f3ce5dd8f8297f449c5f8d
shellyOei <oeishelly15@gmail.com>
2025-12-14 23:03:14 +0700

    chore: change bg color

--- src/features/hr/ScreeningPage.jsx
+++ src/features/hr/ScreeningPage.jsx
@@ -110,7 +110,7 @@ function ScreeningPage() {
 
   return (
     <Layout>
-      <div className="bg-gray-100 min-h-screen p-4 sm:p-6 lg:p-8 font-sans">
+      <div className="bg-blue-50 min-h-screen p-4 sm:p-6 lg:p-8 font-sans">
         <div className="max-w-4xl mx-auto">
 
           <div className="text-center mb-6">
